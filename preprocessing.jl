0e3efbc5831d6f4b6e7d09abf5f4b2227903200c
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-12 22:55:32 +0200

    cleanup + zapis do czystych plików na podstawie których będziemy tworzyć modele

--- preprocessing.jl
+++ preprocessing.jl
@@ -1,6 +1,7 @@
 using CSV
 using DataFrames
 using Dates
+using ScientificTypes
 
 include("preprocessing_helpers.jl")
 include("dictionaries/flagi_lokacje.jl")
@@ -11,6 +12,7 @@ df_pp = loader("dane pierwotne/pomiary pogodowe.csv")
 
 # wstępny preprocessing
 rename!(df_ze, lowercase.(names(df_ze)))
+
 if "hour ending" in names(df_ze)
     rename!(df_ze, "hour ending" => "datetime")
 end
@@ -19,7 +21,6 @@ dropmissing!(df_pp)
 
 df_ze.datetime = DateTime.(df_ze.datetime, dateformat"mm/dd/yyyy HH:MM")
 
-names(df_ze)
 
 columns = [:coast, :east, :fwest, :north, :ncent, :south, :scent, :west, :ercot]
 
@@ -32,7 +33,7 @@ df_pp.prcp_trace = [get(trace_dict, String(x), false)      for x in df_pp.prcp_a
 df_pp.snow_trace = [get(trace_dict, String(x), false)      for x in df_pp.snow_attributes]
 df_pp.location   = [get(location_dict, String(x), missing) for x in df_pp.name]    
 
-#usunięcie kolumn
+#usunięcie kolumn  
 cols_to_delete_pp = [:awnd_attributes, :tmax_attributes, :tmin_attributes, :prcp_attributes, :snow_attributes, :name, :station, :latitude, :longitude, :elevation]
 select!(df_pp, Not(cols_to_delete_pp))
 
@@ -40,7 +41,7 @@ select!(df_pp, Not(cols_to_delete_pp))
 rename!(df_pp, "awnd" => "wind_speed")
 rename!(df_pp, "prcp" => "precipitation")
 rename!(df_pp, "snow" => "snowfall")
-    
+
 rename!(df_pp, "awnd_trace" => "t_wind_speed")
 rename!(df_pp, "prcp_trace" => "t_precipitation")
 rename!(df_pp, "snow_trace" => "t_snowfall")
@@ -49,7 +50,6 @@ rename!(df_pp, "tmax" => "max_temp")
 rename!(df_pp, "tmin" => "min_temp")
 
 #zmiana kolejności
-select!(df_pp, [:date, :location, :precipitation, :t_precipitation, :wind_speed, :t_wind_speed, :snowfall, :t_snowfall, :max_temp, :min_temp])
 
 
 #!SECTION - TODO 
@@ -58,3 +58,14 @@ select!(df_pp, [:date, :location, :precipitation, :t_precipitation, :wind_speed,
 # AUSTIN - SCENT
 # SAN ANTONIO - SCENT
 # ew. ercot jako dodatkowa miara, lub w ogóle zmienna opisywana
+
+
+
+
+
+
+select!(df_pp, [:date, :location, :precipitation, :t_precipitation, :wind_speed, :t_wind_speed, :snowfall, :t_snowfall, :max_temp, :min_temp])
+
+# zapis
+# CSV.write("dane przetworzone/pomiary pogodowe.csv", df_pp)
+# CSV.write("dane przetworzone/zapotrzebowanie energetyczne.csv", df_ze)

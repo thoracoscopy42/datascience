d6ed8c7bf0536237c75b9aa324a9b57207b5ce6d
Julia <niraxicani@gmail.com>
2026-05-15 16:53:22 +0200

    badanie rozkładów danych

--- plots/plots.jl
+++ plots/plots.jl
@@ -9,18 +9,32 @@ df = CSV.read("../data/partial/weather/austin.csv", DataFrame)
 
 df.date = Date.(df.date)
 
+# fig = Figure(size = (700, 700))
+
+# ax = Axis(
+#     fig[1,1], 
+#     title = "Rozkład temperatury maksymalnej i minimalnej w czasie",
+#     xlabel = "Data",
+#     ylabel = "Temperatura",
+# )
+
+# lines!(ax, df.date, df.max_temp, label = "MAX", color = "#DB3B0F")
+# lines!(ax, df.date, df.min_temp, label = "MIN", color = "#56C3DB")
+# axislegend(ax)
+# save("Wykres temperatury.svg", fig) 
+
+# fig
+
 fig = Figure(size = (700, 700))
 
 ax = Axis(
     fig[1,1], 
-    title = "Rozkład temperatury maksymalnej i minimalnej w czasie",
-    xlabel = "Data",
-    ylabel = "Temperatura",
+    title = "Zależność maksymalnej temperatury od wiatru",
+    xlabel = "Temperatura maksymalna",
+    ylabel = "Prędkość wiatru",
 )
+density!(ax, df.snowfall)
 
-lines!(ax, df.date, df.max_temp, label = "MAX")
-lines!(ax, df.date, df.min_temp, label = "MIN")
-axislegend(ax)
-save("Wykres temperatury.svg", fig) 
+save("Wykres temperatury do prędkości wiatru.svg", fig)
 
 fig

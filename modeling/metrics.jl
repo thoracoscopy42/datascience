eddc02282a03ccfdb4b281a8aecef4e69ada8d22
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-17 22:45:15 +0200

    dodane jeszcz mape do miar

--- modeling/metrics.jl
+++ modeling/metrics.jl
@@ -8,6 +8,16 @@ function rmse(y_true, y_pred)
     return sqrt(mean((y_true .- y_pred) .^ 2))
 end
 
+function mape(y_true, y_pred)
+    valid_idx = y_true .!=0
+
+    if sum(valid_idx) == 0
+        return missing
+    end
+
+    return mean(abs.((y_true[valid_idx] .- y_pred[valid_idx]) ./ y_true[valid_idx])) * 100
+end
+
 function r2_score(y_true, y_pred)
 
     res = sum((y_true .- y_pred) .^ 2)
@@ -21,6 +31,7 @@ function regression_report(y_true, y_pred)
     return (
         MAE  = mae(y_true, y_pred),
         RMSE = rmse(y_true, y_pred),
+        MAPE = mape(y_true, y_pred),
         R2   = r2_score(y_true, y_pred)
     )
     
